80f8db0bdaa2950224caa9e5aecaada0955f325b
ZALAK THAKOR <72291815+zalakthakor@users.noreply.github.com>
2022-05-25 16:26:53 +0530

    Update user.js

--- MERN-Ticket/Backend/models/user.js
+++ MERN-Ticket/Backend/models/user.js
@@ -5,7 +5,7 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true },
   password: { type: String, required: true },
   id: { type: String },
-  phone:{type:Number,required: true}
+  
 });
 
 export default mongoose.model("User", userSchema);
