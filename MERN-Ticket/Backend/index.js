84cc3c5c8d5d58d86e783b848da5dfb1c6d1cc0c
ZALAK THAKOR <72291815+zalakthakor@users.noreply.github.com>
2022-05-25 16:28:34 +0530

    Update index.js

--- MERN-Ticket/Backend/index.js
+++ MERN-Ticket/Backend/index.js
@@ -15,10 +15,10 @@ app.use("/user", userRouter);
 app.use('/tickets',ticketRouter);
 
 
-const pORT = process.env.PORT|| 5000;
+const PORT = process.env.PORT|| 5000;
 const uri ="mongodb://localhost:27017/MERNTicket";
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(pORT, () => console.log(`Server Running on Port: http://localhost:${pORT}`)))
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
 
