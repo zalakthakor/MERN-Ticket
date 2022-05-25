62ca6ed39f3736c378adbaa34534cb64a18dfc22
ZALAK THAKOR <72291815+zalakthakor@users.noreply.github.com>
2022-05-25 16:22:05 +0530

    Update auth.js

--- MERN-Ticket/Backend/middleware/auth.js
+++ MERN-Ticket/Backend/middleware/auth.js
@@ -1,29 +1,21 @@
 import jwt from "jsonwebtoken";
-
-const secret = 'test';
-
+const secret=process.env.SECRET;
 const auth = async (req, res, next) => {
   try {
-  
     const token = req.headers.authorization.split(" ")[1];
-    const isCustomAuth = token.length < 500;
-  
     let decodedData;
 
-    if (token && isCustomAuth) {      
+    if (token) {      
       decodedData = jwt.verify(token, secret);
-
       req.userId = decodedData?.id;
-    } else {
-      decodedData = jwt.decode(token);
-
-      req.userId = decodedData?.sub;
-    }    
+      }
 
     next();
   } catch (error) {
- 
+    
   }
 };
+
+
 
 export default auth;
